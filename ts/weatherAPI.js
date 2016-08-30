dc6d4ddde88821ecde888efd8575621ac9db8361
Kaimin Li <boknoobli2@hotmail.com>
2016-08-30 23:24:21 +1200

    Simple dropdown added, tested API call with a city.

---
+++ ts/weatherAPI.js
@@ -0,0 +1,10 @@
+$.get("http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=json&APPID=ea030e80b21e4006ed84c9781fa9c3a4", function (data) {
+    console.log(data);
+    $("#weatherRaw").html(data.city.name + " "
+        + data.list[0].dt_txt + " "
+        + Math.round((data.list[0].main.temp_max - 273.15) * 10) / 10);
+});
+$(document).ready(function () {
+    $('.menu').dropit();
+});
+//# sourceMappingURL=weatherAPI.js.map
