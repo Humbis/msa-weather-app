7292df40dde247fa82c15a577f2db0aa13ef85ba
Kaimin Li <boknoobli2@hotmail.com>
2016-08-31 16:09:08 +1200

    added a simple search feature and dynamic weather info

--- ts/weatherAPI.ts
+++ ts/weatherAPI.ts
@@ -1,12 +1,63 @@
-$.get("http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=json&APPID=ea030e80b21e4006ed84c9781fa9c3a4",
-    function (data) {
-        console.log(data);
-        $("#weatherRaw").html(data.city.name + " " 
-            + data.list[0].dt_txt + " " 
-            + Math.round((data.list[0].main.temp_max - 273.15) * 10) / 10);
-    }
-);
+
+// class weatherData{
+//     name: string;
+//     temp: number;
+//     time: string;
+//     weatherCond: string;
+//     clouds: number;
+//     wind: number;
+//     rain: number;
+
+//     constructor(city, temp, time, weatherCond, clouds, wind){
+//         this.name = name;
+//         this.temp = temp;
+//         this.time = time;
+//         this.weatherCond = weatherCond;
+//         this.clouds = clouds;
+//         this.wind = wind;
+//     }
+// }
+declare var $;
+
+
+$('#searchbtn').click(function () {
+    var cityName: string = $('#cities').val();
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=ea030e80b21e4006ed84c9781fa9c3a4",
+        function (data) {
+            console.log(data);
+            $("#weatherRaw").html(data.name + " " 
+                + Math.round((data.main.temp - 273.15) * 10) / 10);
+        }
+    )
+})
 
 $(document).ready(function() {
     $('.menu').dropit();
+    var availableTags = [
+      "Auckland",
+      "Wellington",
+      "Christchurch",
+      "Hamilton",
+      "Tauranga",
+      "Dunedin",
+      "Nelson",
+      "Rotorua",
+      "Whangarei",
+      "Invercargill",
+      "Whanganui",
+      "Gisborne",
+      "London",
+      "New York",
+      "Washington",
+      "Melbourne",
+      "Adelaide",
+      "Brisbane",
+      "Sydney",
+      "Canberra",
+      "Los Angeles",
+      "Chicago"
+    ];
+    $( "#cities" ).autocomplete({
+      source: availableTags
+    });
 });
